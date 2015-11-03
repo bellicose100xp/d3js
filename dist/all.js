adb4442ef3d8837323941fe5187f0b108f9572eb
bellicose <bellicose.agnostic@gmail.com>
2015-11-03 08:37:35 -0800

    line graph

--- dist/all.js
+++ dist/all.js
@@ -1,3 +1,52 @@
 'use strict';
 
-d3.select('body').append('h1').text('Welcome to d3');
+var h = 100;
+var w = 400;
+var ds = undefined;
+var total = undefined;
+var average = undefined;
+
+d3.csv('MonthlySales.csv', function (error, data) {
+
+    if (error) {
+        console.log(error);
+    }
+
+    if (data) {
+        ds = data;
+    }
+
+    var buildLines = function buildLines() {
+        var lineFun = d3.svg.line().x(function (d) {
+            return (d.month - 20130001) / 3.2;
+        }).y(function (d) {
+            return h - d.sales;
+        }).interpolate('linear');
+
+        var svg = d3.select('body').append('svg').attr({ width: w, height: h }).append('path').attr({
+            d: lineFun(ds),
+            fill: 'none',
+            stroke: 'purple',
+            'stroke-width': 2
+        });
+    };
+
+    var showTotals = function showTotals() {
+        var metrics = [];
+
+        var total = ds.reduce(function (prev, curr) {
+            return prev + Number(curr.sales);
+        }, 0);
+        metrics.push('Total: ' + total);
+
+        average = total / ds.length;
+        metrics.push('Average: ' + average.toFixed(2));
+
+        d3.select('body').append('table').selectAll('tr').data(metrics).enter().append('tr').append('td').text(function (d) {
+            return d;
+        });
+    };
+
+    buildLines();
+    showTotals();
+});
---
+++ src/csv.js
@@ -0,0 +1,55 @@
+'use strict';
+let h = 100;
+let w = 400;
+let ds;
+let total;
+let average;
+
+d3.csv('MonthlySales.csv', (error, data) => {
+
+    if (error) {
+        console.log(error);
+    }
+
+    if (data) {
+        ds = data;
+    }
+
+    let buildLines = () => {
+        let lineFun = d3.svg.line()
+            .x(d => (d.month - 20130001) / 3.2)
+            .y(d => h - d.sales)
+            .interpolate('linear');
+
+        let svg = d3.select('body').append('svg').attr({width: w, height: h})
+            .append('path')
+            .attr({
+                d: lineFun(ds),
+                fill: 'none',
+                stroke: 'purple',
+                'stroke-width': 2
+            })
+    };
+
+    let showTotals = () => {
+        let metrics = [];
+
+        let total = ds.reduce((prev, curr) => prev + Number(curr.sales), 0);
+        metrics.push(`Total: ${total}`);
+
+        average = total / ds.length;
+        metrics.push(`Average: ${average.toFixed(2)}`);
+
+        d3.select('body').append('table')
+            .selectAll('tr')
+            .data(metrics)
+            .enter()
+            .append('tr')
+            .append('td')
+            .text(d => d);
+    };
+
+    buildLines();
+    showTotals();
+
+});
